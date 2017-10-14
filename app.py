1ec4e7c7be7612fb23ead7fb7f9d5a5f6d4e8e56
kevintian <kevintian@gatech.edu>
2017-10-14 02:26:36 -0400

    Added routing to display data viz

--- app.py
+++ app.py
@@ -9,5 +9,9 @@ def index():
 	#password= str(request.form['password'])
     return render_template('index.html', title="Index")
 
+@app.route('/visualization')
+def graph():
+    return render_template("graph.html", title = "Struct Graph")
+
 if __name__ == "__main__":
     app.run(debug=True)
